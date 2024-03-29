8abb953a1543ff453acaf10244c79e631a9595d7
Harsh <harsh77@Harshs-MacBook-Pro.local>
2024-03-29 18:54:54 +0530

    Added namespace

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 class Configs(object):
     # SQLALCHEMY_DATABASE_URI = "postgresql://cool_user:1234@localhost:5432/cool_db"
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@db:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@db/postgres"
     # SQLALCHEMY_DATABASE_URI="postgresql://postgres:postgrespw@localhost:55004"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET="SECRET"
